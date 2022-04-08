6379d1c127fec3e46608a67e9eb64e583a337e35
Daniel Isenmann <daniel.isenmann@gmail.com>
2022-04-09 01:29:06 +0200

    Fixed bad design in navigaton bar after update

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
+
+import './NavMenu.css'
 
 function NavMenu() {
   const [collapsed, setCollapsed] = useState(true);
@@ -13,7 +14,6 @@ function NavMenu() {
   return (
     <header>
       <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-        <Container>
           <NavbarBrand tag={Link} to="/">Liverpool</NavbarBrand>
           <NavbarToggler onClick={toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
@@ -23,7 +23,6 @@ function NavMenu() {
               </NavItem>
             </ul>
           </Collapse>
-        </Container>
       </Navbar>
     </header>
   );
